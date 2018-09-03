5aac6acb807f5c0d76571a528b18de22b740c71c
Imran Kaderbhai <imrankaderbhai@googlemail.com>
2018-09-03 20:43:49 +0100

    first input made and output

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld msg="Pooh"/>
+    <Input/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Input from './components/Input.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Input
   }
 }
 </script>
---
+++ src/components/Input.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <input v-model="todo" placeholder="Touch me" >
+    <p>To Do list:{{todo}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Input',
+  data: function() {
+    let todo = ''
+    return {
+      todo : todo
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
