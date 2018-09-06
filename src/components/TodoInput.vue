2d290c5d20165e0079c1af70fa9d9e20f1187a34
malfernion <malfernion@gmail.com>
2018-09-06 20:03:23 +0100

    Put new input in place and set up todo item model and data passing

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="todoInput">
+    <md-field md-inline>
+      <label>What do you need to do?</label>
+      <md-input v-model="todo" @keyup.enter="addTodoItem"></md-input>
+    </md-field>
+    <md-button class="md-raised md-primary" @click="addTodoItem">Add</md-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoInput',
+  data: function() {
+    let todo = ''
+    return {
+      todo : todo
+    }
+  },
+  methods: {
+    addTodoItem: function () {
+      if(this.todo)
+      this.$emit('add-todo-item',
+      {
+        todoText: this.todo,
+        done: false
+      });
+      this.todo = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #todoInput {
+    display: flex;
+  }
+
+  #todoInput .md-field {
+    flex-grow: 1;
+    margin-right: 30px;
+  }
+
+  #todoInput button {
+    line-height: 24px;
+  }
+</style>
